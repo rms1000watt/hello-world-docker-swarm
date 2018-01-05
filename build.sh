5aa6af181a11b86833e28787755c04dc3a445760
Ryan Smith <rms1000watt@gmail.com>
2018-01-05 10:34:51 -0800

    working pg

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker build \
-  --build-arg GO_DOCKER_VERSION=${GO_DOCKER_VERSION:-1.9.2-alpine3.7} \
+  --build-arg GO_DOCKER_VERSION=${GO_DOCKER_VERSION:-1.9.2} \
   --force-rm \
   --no-cache \
   --compress \
